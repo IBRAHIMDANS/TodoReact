d77e880ba3001c0559a735521c8d44eec63bad55
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-08-13 17:25:15 +0200

    move test app

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('check title', () => {
   const { getByText } = render(<App />);
